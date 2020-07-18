3876405e06753b4c7454163512484908fde0b684
Vladimir <vladimirdd96@gmail.com>
2020-07-18 11:52:12 +0300

    navScript fix

---
+++ views/includes/navScript.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('menuu');
+const toggle = document.querySelector('navbar-toggler');
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('show');
+
+})
